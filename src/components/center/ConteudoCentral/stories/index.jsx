d0ecf9731354efd9e11da2beb30e7a1965afc5ee
Gustavo Sales <gussales14@gmail.com>
2024-05-01 03:10:31 -0300

    Mudanças no campo de story
    Adicionado campo separado de 'meu story', borda colorida na foto, tanto padronizado como a borda do 'closed friends'

--- src/components/center/ConteudoCentral/stories/index.jsx
+++ src/components/center/ConteudoCentral/stories/index.jsx
@@ -5,19 +5,38 @@ import './style.css'
 const users = [
   {
     name: "Gustavo Sales",
-    image_url: "https://github.com/Gusales.png"
+    image_url: "https://github.com/Gusales.png",
+    isClosedFriends: false,
   },
   {
     name: "Gabriel Azevedo",
-    image_url: "https://github.com/gabaugusto.png"
+    image_url: "https://github.com/gabaugusto.png",
+    isClosedFriends: true,
   },
   {
     name: "Gabriel Braz",
-    image_url: "https://github.com/bieelbrz.png"
+    image_url: "https://github.com/bieelbrz.png",
+    isClosedFriends: false,
   },
   {
     name: "Lucas Tomaz",
-    image_url: "https://github.com/LucasLTM.png"
+    image_url: "https://github.com/LucasLTM.png",
+    isClosedFriends: false,
+  },
+  {
+    name: "Emy",
+    image_url: "https://github.com/EmillyMLFreitas.png",
+    isClosedFriends: true,
+  },
+  {
+    name: "Diogo Antonny",
+    image_url: "https://github.com/DiogoJP202.png",
+    isClosedFriends: false,
+  },
+  {
+    name: "Gerson Silva",
+    image_url: "https://github.com/G3rsonBR.png",
+    isClosedFriends: false,
   },
 ]
 
@@ -26,7 +45,7 @@ export function Stories() {
     <header className="content_header">
       {
         users.map((user, index) => {
-          return <Story name={user.name} image_url={user.image_url} key={index} />
+          return <Story name={user.name} image_url={user.image_url} key={index} myStory={index === 0} isClosedFriends={user.isClosedFriends} />
         })
       }
     </header>
--- src/components/center/ConteudoCentral/story/index.jsx
+++ src/components/center/ConteudoCentral/story/index.jsx
@@ -1,10 +1,18 @@
+import { FaPlus } from "react-icons/fa";
 import './style.css'
 
 export function Story(props) {
   return(
-    <section className="story_field">
-      <img src={props.image_url} className="profile_image" alt={`Imagem de perfil de ${props.name}`} />
-      <p className="profile_name">{props.name}</p>
+    <section className={`story_field ${props.myStory ? 'my_story' : ''}`}>
+      <div className={`story_image ${props.myStory && 'post_new_story'} ${props.isClosedFriends && 'close-friends'}`}>
+        <img src={props.image_url} className="profile_image" alt={`Imagem de perfil de ${props.name}`} />
+        {props.myStory && (
+          <div className="icon_plus">
+            <FaPlus className="icon_add_story" fontSize={12} color="#ffffff" />
+          </div>
+        )}
+      </div>
+      <p className="profile_name">{props.myStory ? 'Seu story' : props.name}</p>
     </section>
   )
 }
